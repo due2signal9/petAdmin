356448d83230bf0d8ca586cc4d95825e5ec9a287
郭赞华 <guozanhua@guozanhuadeMac-mini.local>
2018-03-22 13:11:30 +0800

    aa
    aa

--- user/models.py
+++ user/models.py
@@ -13,7 +13,7 @@ class PetUser(models.Model):
     password = models.CharField(u'密码', max_length=256, blank=True)
     gender = models.SmallIntegerField(u'性别', default=0)
     signature = models.CharField(u'个性签名', max_length=32, blank=True)
-    last_login = models.DateTimeField(u'上次登录')
+    last_login = models.DateTimeField(u'上次登录', blank=True)
     is_deleted = models.SmallIntegerField(u'删除状态', default=0)
     is_blocked = models.SmallIntegerField(u'被禁状态', default=0)
     time_created = models.DateTimeField(u'创建时间', auto_now_add=True)
